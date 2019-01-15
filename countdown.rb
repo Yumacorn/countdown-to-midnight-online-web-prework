bcb3fc808160a13f583fb41c422f4c28a735fc1f
Ryan Mui <reachryanmui@gmail.com>
2019-01-15 04:41:41 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  loop do
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    break if seconds == 0
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+countdown(10)
+
+def countdown_with_sleep(seconds)
+  loop do
+    puts "#{seconds} SECOND(S)!"
+    sleep(1)
+    seconds -= 1
+    break if seconds == 0
+  end
+  return "HAPPY NEW YEAR!"
 end
