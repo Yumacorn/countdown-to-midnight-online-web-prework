6b492c499cd00440cbe93daac101ee4dd71fa28e
Ryan Mui <reachryanmui@gmail.com>
2019-01-15 04:41:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,7 +12,11 @@ end
 countdown(10)
 
 def countdown_with_sleep(seconds)
-  if seconds >= 5 do
-    countdown(seconds)
+  loop do
+    puts "#{seconds} SECOND(S)!"
+    sleep(1)
+    seconds -= 1
+    break if seconds == 0
   end
+  return "HAPPY NEW YEAR!"
 end
